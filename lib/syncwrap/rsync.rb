5dafb91ab18717eb06002449db3a12016eca4c89
David Kellum <dek-oss@gravitext.com>
2014-01-17 17:52:43 -0800

    Fix rsync header

--- lib/syncwrap/rsync.rb
+++ lib/syncwrap/rsync.rb
@@ -12,10 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
 # implied.  See the License for the specific language governing
 # permissions and limitations under the License.
-#
-# The non-blocking implementation of capture below was derived from
-# rake-remote_task, MIT License:
-# Copyright (c) Ryan Davis, RubyHitSquad
 #++
 
 require 'syncwrap/base'
