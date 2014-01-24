14d646da933883872062e7d342f38b249901ed12
David Kellum <dek-oss@gravitext.com>
2014-01-23 16:55:27 -0800

    Initial conversion of PostgreSQL to component

--- lib/syncwrap/components/postgresql.rb
+++ lib/syncwrap/components/postgresql.rb
@@ -14,120 +14,103 @@
 # permissions and limitations under the License.
 #++
 
-require 'syncwrap/distro'
-require 'syncwrap/ubuntu'
-require 'syncwrap/rhel'
+require 'syncwrap/component'
+require 'syncwrap/components/rhel'
+require 'syncwrap/components/ubuntu'
 
 # Provisions for install and configuration of PostgreSQL
-module SyncWrap::PostgreSQL
-  include SyncWrap::Distro
+module SyncWrap
 
-  # Location of postgresql data dir (databases + config in default
-  # case)
-  attr_accessor :pg_data_dir
+  class PostgreSQL < Component
 
-  # The stock distribution default data_dir. Difference with
-  # pg_data_dir triggers additional install steps.
-  attr_accessor :pg_default_data_dir
+    # Location of postgresql data dir
+    attr_accessor :pg_data_dir
 
-  # Local directory for configuration files (different by
-  # distribution)
-  attr_accessor :pg_deploy_config
-
-  def initialize
-    super
-    @pg_data_dir = '/pg/data'
-  end
-
-  def pg_config_dir
-    @pg_data_dir
-  end
-
-  def pg_install
-    dist_install 'postgresql'
-  end
-
-  # Update PostgreSQL config files
-  def pg_configure
-    rput( "#{pg_deploy_config}/", pg_config_dir, :user => 'postgres' )
-    sudo( "chmod 700 #{pg_data_dir}" ) if pg_config_dir == pg_data_dir
-  end
-
-  def pg_start
-    dist_service( 'postgresql', 'start' )
-  end
-
-  def pg_stop
-    dist_service( 'postgresql', 'stop' )
-  end
-
-  def self.included( base )
-    if base.include?( SyncWrap::RHEL )
-      base.send( :include, SyncWrap::PostgreSQL::RHEL )
-    elsif base.include?( SyncWrap::Ubuntu )
-      base.send( :include, SyncWrap::PostgreSQL::Ubuntu )
-    end
-  end
-
-  module RHEL
-
-    def initialize
+    def initialize( opts = {} )
+      @pg_data_dir = '/pg/data'
       super
-      #Per Amazon Linux 2012 3.3
-      @pg_default_data_dir = '/var/lib/pgsql9/data'
-      @pg_deploy_config = 'postgresql/rhel'
     end
 
-    def pg_install
-      super
-      unless @pg_data_dir == @pg_default_data_dir
-        # (Per Amazon Linux)
-        # Install PGDATA var override for init.d/postgresql
-        rput( 'etc/sysconfig/pgsql/postgresql', :user => 'root' )
-        sudo <<-SH
-          mkdir -p #{pg_data_dir}
-          chown postgres:postgres #{pg_data_dir}
-          chmod 700 #{pg_data_dir}
-        SH
+    def pg_deploy_config
+      case distro
+      when RHEL
+        'postgresql/rhel'
+      when Ubuntu
+        'postgresql/ubuntu'
+      else
+        raise "Distro #{distro.class.name} not supported"
       end
-      dist_service( 'postgresql', 'initdb' )
-    end
-
-  end
-
-  module Ubuntu
-
-    def initialize
-      super
-      @pg_default_data_dir = '/var/lib/postgresql/9.1/main'
-      @pg_deploy_config = 'postgresql/ubuntu'
-    end
-
-    def pg_install
-      super
-      pg_stop #Ubuntu does a start
-      pg_adjust_sysctl
-      pg_relocate unless @pg_data_dir == @pg_default_data_dir
     end
 
     def pg_config_dir
-      "/etc/postgresql/9.1/main"
+      case distro
+      when RHEL
+        pg_data_dir
+      when Ubuntu
+        '/etc/postgresql/9.1/main'
+      else
+        raise "Distro #{distro.class.name} not supported"
+      end
     end
 
-    def pg_adjust_sysctl
-      rput( 'etc/sysctl.d/61-postgresql-shm.conf', :user => 'root' )
-      sudo "sysctl -p /etc/sysctl.d/61-postgresql-shm.conf"
+    def install
+      package_install
+      setup_data_dir
+      pg_configure
+      pg_start
     end
 
-    # Move the data dir into its final location, since on Ubuntu the
-    # package install does the initdb step
-    def pg_relocate
-      sudo <<-SH
-        mkdir -p #{pg_data_dir}
-        chown postgres:postgres #{pg_data_dir}
-        chmod 700 #{pg_data_dir}
-        mv #{pg_default_data_dir}/* #{pg_data_dir}/
-      SH
+    def package_install
+      dist_install 'postgresql'
+      if distro.is_a?( Ubuntu )
+        pg_stop
+        rput( 'etc/sysctl.d/61-postgresql-shm.conf', :user => 'root' )
+        sudo "sysctl -p /etc/sysctl.d/61-postgresql-shm.conf"
+      end
+    end
+
+    def setup_data_dir
+      case distro
+
+      when RHEL
+        unless pg_data_dir == '/var/lib/pgsql9/data'
+          # (Per Amazon Linux)
+          # Install PGDATA var override for init.d/postgresql
+          rput( 'etc/sysconfig/pgsql/postgresql.erb', :user => 'root' )
+          sudo <<-SH
+            mkdir -p #{pg_data_dir}
+            chown postgres:postgres #{pg_data_dir}
+            chmod 700 #{pg_data_dir}
+          SH
+        end
+        dist_service( 'postgresql', 'initdb' )
+
+      when Ubuntu
+         unless pg_data_dir == '/var/lib/postgresql/9.1/main'
+           sudo <<-SH
+             mkdir -p #{pg_data_dir}
+             chown postgres:postgres #{pg_data_dir}
+             chmod 700 #{pg_data_dir}
+             mv #{pg_default_data_dir}/* #{pg_data_dir}/
+           SH
+         end
+      else
+        raise "Distro #{distro.class.name} not supported"
+      end
+    end
+
+    # Update PostgreSQL config files
+    def pg_configure
+      rput( "#{pg_deploy_config}/", pg_config_dir, :user => 'postgres' )
+      sudo( "chmod 700 #{pg_data_dir}" ) if pg_config_dir == pg_data_dir
+    end
+
+    def pg_start
+      dist_service( 'postgresql', 'start' )
+    end
+
+    def pg_stop
+      dist_service( 'postgresql', 'stop' )
     end
 
   end
