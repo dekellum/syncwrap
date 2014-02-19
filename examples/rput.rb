dc69488d79def36e7b1cab7070bf6fe67ac0a713
David Kellum <dek-oss@gravitext.com>
2014-02-19 11:29:46 -0800

    rdoc typo

--- examples/rput.rb
+++ examples/rput.rb
@@ -1,7 +1,7 @@
 # Important: Make this examples/sync directory first in
 # sync_paths. This is not done by default, to make sure its explicit.
 #
-# The second path is the SyncWrap gems own sync directory.  The first
+# The second path is the SyncWrap gem's own sync directory.  The first
 # path can effectively override any file that would otherwise be found
 # in the second. This includes being able to override a static file
 # with a template, or the other way around. This is what we mean by
