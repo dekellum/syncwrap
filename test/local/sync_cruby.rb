041e8c0a3f4658bc08dc42d16e70dbd65106b3cb
David Kellum <dek-oss@gravitext.com>
2014-02-21 09:51:54 -0800

    Add test/local/sync_cruby.rb

---
+++ test/local/sync_cruby.rb
@@ -0,0 +1,22 @@
+#--
+# Copyright (c) 2011-2014 David Kellum
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you
+# may not use this file except in compliance with the License.  You may
+# obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.  See the License for the specific language governing
+# permissions and limitations under the License.
+#++
+
+include SyncWrap
+
+role( :ruby, CRubyVM.new( ruby_version: "2.0.0-p353" ) )
+
+host( 'centos-1', RHEL.new,   Network.new, :ruby, internal_ip: '192.168.122.4' )
+host( 'ubuntu-1', Ubuntu.new, Network.new, :ruby, internal_ip: '192.168.122.145' )
