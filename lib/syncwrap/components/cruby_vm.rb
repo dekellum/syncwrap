02069abc7a868ca2097b046823328263fe0f9095
David Kellum <dek-oss@gravitext.com>
2014-07-07 21:22:44 -0700

    Add ruby 2.1 to uninstall_distro_ruby

--- lib/syncwrap/components/cruby_vm.rb
+++ lib/syncwrap/components/cruby_vm.rb
@@ -96,9 +96,9 @@ module SyncWrap
 
     def uninstall_distro_ruby
       if distro.is_a?( RHEL )
-        dist_uninstall( %w[ ruby ruby18 ruby19 ruby20 ] )
+        dist_uninstall( %w[ ruby ruby18 ruby19 ruby20 ruby21 ] )
       else
-        dist_uninstall( %w[ ruby ruby1.8 ruby1.9 ruby1.9.1 ruby1.9.3 ruby2.0 ] )
+        dist_uninstall( %w[ ruby ruby1.8 ruby1.9 ruby1.9.1 ruby1.9.3 ruby2.0 ruby2.1 ] )
       end
     end
 
