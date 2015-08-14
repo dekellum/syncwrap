5c30cbdc4cc94b68c61d9bd97d60ba4bc289c6f0
David Kellum <dek-oss@gravitext.com>
2015-08-14 12:01:28 -0700

    Move AmazonEC2.ec2_user_data to UserData module

---
+++ lib/syncwrap/user_data.rb
@@ -0,0 +1,38 @@
+#--
+# Copyright (c) 2011-2015 David Kellum
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you
+# may not use this file except in compliance with the License.  You may
+# obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.  See the License for the specific language governing
+# permissions and limitations under the License.
+#++
+
+module SyncWrap
+
+  # Utility methods for generating scripts to pass as user data.
+  module UserData
+
+    private
+
+    # Returns an sh script to allow no password, no tty sudo for a
+    # specified user by writing a file to /etc/sudoers.d/<user>
+    def no_tty_sudoer( user )
+      script = <<-SH
+        #!/bin/sh -e
+        echo '#{user} ALL=(ALL) NOPASSWD:ALL'  > /etc/sudoers.d/#{user}
+        echo 'Defaults:#{user} !requiretty'   >> /etc/sudoers.d/#{user}
+        chmod 440 /etc/sudoers.d/#{user}
+      SH
+      script.split( "\n" ).map( &:strip ).join( "\n" )
+    end
+
+  end
+
+end
