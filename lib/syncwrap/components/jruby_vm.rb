c68a56ad579d0882e0e369f777de088cc32702d8
David Kellum <dek-oss@gravitext.com>
2017-07-06 13:29:34 -0700

    Add jruby 9.1.12.0 to KNOWN_HASHES

--- lib/syncwrap/components/jruby_vm.rb
+++ lib/syncwrap/components/jruby_vm.rb
@@ -44,6 +44,8 @@ module SyncWrap
       '1.7.25' => 'cd15aef419f97cff274491e53fcfb8b88ec36785',
       '1.7.26' => 'cca25a1ffb8b75a8d4a4d4667e7f6b20341c2b74',
       '1.7.27' => '4a24fe103d3735b23cc58668dec711857125a6f3',
+      '9.1.12.0' =>
+      'ddb23c95f4b3cc3fc1cc57b81cb4ceee776496ede402b9a6eb0622cf15e1a597',
     }
 
     # JRuby version to install (default: DEFAULT_VERSION)
